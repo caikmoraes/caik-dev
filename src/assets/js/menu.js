7189753654eaca3dd6494d076847963ef89960b5
Caik Moraes <caikmoraes@pop-os.localdomain>
2020-12-23 16:44:15 -0300

    Término do esboço do menu

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,7 +1,57 @@
-function menuToggle() {
-    const header = document.querySelector('header')
-    const menu = document.querySelector('nav')
-    header.style.marginLeft = menu.offsetWidth + 'px'
+function closeByLink() {
+    if(screen.width < 480) {
+        const toggle = document.querySelector('[c-toggle="close"]')
+        const nav = document.querySelector('nav')
+        document.querySelectorAll('[c-link]').forEach(link => {
+            link.onclick = () => {
+                closeMenu(toggle, nav)
+                disableScroll(false)
+            }
+        })
+    }
 }
 
+function openMenu(toggleOpen, nav) {
+    const toggleClose = document.querySelector('[c-toggle="close"]')
+    toggleClose.style.display = 'block'
+    toggleOpen.style.display = 'none'
+    nav.style.left = 0
+}
+
+function closeMenu(toggleClose, nav) {
+    const toggleOpen = document.querySelector('[c-toggle="open"]')
+    toggleClose.style.display = 'none'
+    toggleOpen.style.display = 'block'
+    nav.style.left = -100 + 'vw'
+}
+
+function disableScroll(disable) {
+    const page = document.querySelector('main')
+    if(disable) {
+        page.style.overflowY = 'hidden'
+    }
+    else {
+        page.style.overflowY = 'scroll'
+    }
+}
+
+function menuToggle() {
+    const toggles = document.querySelectorAll('[c-toggle]')
+    const nav = document.querySelector('nav')
+    toggles.forEach(toggle => {
+        toggle.onclick = () => {
+            const toggleValue = toggle.attributes['c-toggle'].value
+            if (toggleValue == 'open') {
+                openMenu(toggle, nav)
+                disableScroll(true)
+            }
+            else {
+                closeMenu(toggle, nav)
+                disableScroll(false)
+            }
+        }
+    })
+}
+
+closeByLink()
 menuToggle()
--- src/assets/js/page.js
+++ src/assets/js/page.js
@@ -1,8 +1,10 @@
-function setMarginLeft() {
-    const menu = document.querySelector('nav')
-    const page = document.querySelector('main')
-
-    page.style.marginLeft = menu.offsetWidth + 'px'
+function alignPageContent() {
+    const nav = document.querySelector('nav')
+    const page  = document.querySelector('.page-content')
+    if(screen.width > 480) {
+        page.style.marginLeft = nav.offsetWidth + 'px'
+    }
+    
 }
-setMarginLeft()
-window.onresize = setMarginLeft
+
+alignPageContent()
