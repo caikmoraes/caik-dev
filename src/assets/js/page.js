cc4fbb64607963fc2f39bbb1c78528fae4bf02f9
Caik Moraes <caikmoraes@pop-os.localdomain>
2020-12-28 16:50:41 -0300

    Estruturação de pagina de skills

--- src/assets/js/page.js
+++ src/assets/js/page.js
@@ -1,8 +1,10 @@
 function alignPageContent() {
     const nav = document.querySelector('nav')
     const page  = document.querySelector('.page-content')
+    const footer = document.querySelector('footer')
     if(screen.width > 1024) {
         page.style.marginLeft = nav.offsetWidth + 'px'
+        footer.style.marginLeft = nav.offsetWidth + 'px'
     }
     
 }
--- src/assets/js/skills.js
+++ src/assets/js/skills.js
@@ -1,6 +1,16 @@
-function getBars() {
-    const percentuals = document.querySelectorAll('ul li .item-content')
-    console.log("Teste: ", percentuals)
+function showOnScroll() {
+    const elements = document.querySelectorAll('[c-skill]')
+    const animationClass = 'animate'
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4)
+    elements.forEach(element => {
+        if (windowTop >= element.offsetTop) {
+            element.classList.add(animationClass)
+        } else {
+            element.classList.remove(animationClass)
+        }
+    })
+
 }
 
-getBars()
+document.addEventListener('DOMContentLoaded', showOnScroll)
+document.addEventListener('scroll', showOnScroll)
